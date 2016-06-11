c12e6204f55b37b6c92dce4466933f3272d75152
Marwan Nakhaleh <mnakhaleh@gmail.com>
2016-06-11 19:38:31 -0400

    yet another HTML5Up template lel

--- agindy/backbone.rb
+++ agindy/backbone.rb
@@ -5,9 +5,13 @@ require 'json'
 set :views, "views"
 
 get "/" do
-	redirect "/heat"
+	erb :index
 end
 
 get "/heat" do
 	erb :heat
+end
+
+get "/geo" do
+	#query = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY"
 end
